33eb39fbb08e4eaab45e139bafc633faa30c5635
Khanh <uefa.euro30@gmail.com>
2021-08-17 16:39:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// b1
+switch (browser) {
+    case 'Edge':
+        alert("You've got the Edge!");
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert('Okay we support these browsers too');
+        break;
+    default:
+        alert('We hope that this page looks ok!');
+}
+// b2
+let a = prompt('a?', '');
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+}
+//b3
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
